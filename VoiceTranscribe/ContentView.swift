5908b0ffdb281984f8820897671cd93eabf68bfc
Gengpu Liu <gpliu3@gmail.com>
2026-02-02 06:49:48 +0800

    Initial Commit

---
+++ VoiceTranscribe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VoiceTranscribe
+//
+//  Created by Gengpu Liu on 2/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VoiceTranscribe/VoiceTranscribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceTranscribeApp.swift
+//  VoiceTranscribe
+//
+//  Created by Gengpu Liu on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct VoiceTranscribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
